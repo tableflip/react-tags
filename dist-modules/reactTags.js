d5e2d722f422b4ecc61182bbbec7706767616d18
Bernard Mordan <bernardmordan@gmail.com>
2016-06-22 09:35:40 +0100

    console logging

--- dist-modules/reactTags.js
+++ dist-modules/reactTags.js
@@ -183,9 +183,9 @@ var ReactTags = React.createClass({
         e.preventDefault();
         var clipboardData = e.clipboardData || window.clipboardData;
         var string = clipboardData.getData('text');
-        var add = this.props.handleAddition;
         string.split(/[ ,]+/).forEach(function (tag) {
-            add(tag);
+            console.log('adding tag', tag, this.props.handleAddition);
+            this.props.handleAddition(tag);
         });
     },
     addTag: function addTag(tag) {
